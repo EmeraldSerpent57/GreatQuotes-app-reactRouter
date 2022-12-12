6dec5067fc77fb91a51c2ba754bb152c1c8e16d3
Alaina Harrison <amvoce1221@gmail.com>
2022-12-12 14:12:48 -0800

    added fallback 'not found' page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+        <div className="centered">
+            <p>Page Not Found!</p>
+        </div>
+    );
+};
+
+export default NotFound;
+
+//Fallback page to show if the URL is not valid
