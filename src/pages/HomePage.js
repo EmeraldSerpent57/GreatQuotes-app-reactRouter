76ae99b8f062f33b344b7453904d6afa9e81348f
Alaina Harrison <amvoce1221@gmail.com>
2023-01-16 15:24:01 -0800

    added more structure to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,29 @@
+import { Fragment } from "react";
 import { Helmet } from "react-helmet";
+import classes from './HomePage.modules.css';
 
 const HomePage = () => {
     return (
-        <>
-            <Helmet>
-                <title>Great Quotes</title>
-                <meta name="description" content="A website where you can view and add quotes that you love"/>
-            </Helmet>
-            <div>
-                <h1>Welcome to Great Quotes!</h1>
-            </div>
-            <div>
-                <p>
-                    Text about what this website does and how to interact with it.
-                </p>
-            </div>
-            <button className="btn">Take me to the quotes!</button>
-        </>
+      <Fragment>
+        <Helmet>
+          <title>Great Quotes</title>
+          <meta
+            name="description"
+            content="A website where you can view and add quotes that you love"
+          />
+        </Helmet>
+        <body className={classes.hpBackground}>
+          <div>
+            <h1>Welcome to Great Quotes!</h1>
+          </div>
+          <div>
+            <p>
+              Text about what this website does and how to interact with it.
+            </p>
+          </div>
+          <button className="btn">Take me to the quotes!</button>
+        </body>
+      </Fragment>
     );
 }
 
